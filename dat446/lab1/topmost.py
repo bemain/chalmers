6f7e472e59582e9e87a250ee3ccbf5c0e65d9c76
bemain <bemain.dev@gmail.com>
2025-01-23 10:30:34 +0100

    Implement lab1

--- dat446/lab1/topmost.py
+++ dat446/lab1/topmost.py
@@ -1,7 +1,27 @@
-import wordfreq, sys
+import sys, urllib.request
+from wordfreq import printTopMost, countWords, tokenize
+
+def fetch_web_file(url: str) -> list[str]:
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf8").splitlines()
 
 def main():
-    pass
+    stop_words: list[str] = []
+    with open(sys.argv[1], encoding="utf-8") as stop_words_file:
+        for line in stop_words_file:
+            stop_words.append(line.strip())
+    
+    lines: list[str]
+    if sys.argv[2].startswith(("https://", "http://")):
+        lines = fetch_web_file(sys.argv[2])
+    else:
+        with open(sys.argv[2], encoding="utf-8") as input_file:
+            lines = input_file.readlines()
+    
+    n: int = int(sys.argv[3])
+    
+    printTopMost(countWords(tokenize(lines), stop_words), n)
+
 
 if __name__ == "__main__":
     main()
--- dat446/lab1/wordfreq.py
+++ dat446/lab1/wordfreq.py
@@ -1,8 +1,52 @@
-def tokenize(lines):
-    pass
+def tokenize(lines: list[str]) -> list[str]:
+    words = []
+    for line in lines:
+        line = line.lower()
 
-def countWords(words, stopWords):
-    pass
+        start = 0
+        end = 0
+        while start < len(line):   
+            if line[start].isspace():
+                # Blanksteg
+                end += 1
 
-def printTopMost(frequencies,n):
-    pass
+            elif line[start].isdigit():
+                # Siffror
+                while end < len(line) and line[end].isdigit():
+                    end += 1
+                words.append(line[start:end])
+                
+
+            elif line[start].isalpha():
+                # Bokstäver
+                while end < len(line) and line[end].isalpha():
+                    end += 1
+                words.append(line[start:end])
+            
+            else:
+                # Tecken
+                words.append(line[start])
+                end += 1
+            
+            start = end            
+    
+    return words
+
+def countWords(words: list[str], stopWords: list[str]) -> dict[str, int]:
+    frequencies = {}
+
+    for word in words:
+        if word in stopWords:
+            continue
+        
+        if not word in frequencies.keys():
+            frequencies[word] = 0
+        frequencies[word] += 1
+
+    return frequencies
+
+def printTopMost(frequencies: dict[str, int], n: int) -> str:
+    frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+    
+    for item in frequencies[:n]:
+        print(item[0].ljust(20) + str(item[1]).rjust(5))
