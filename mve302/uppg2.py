bc6d91beac52ca652d245d4e0aaea9941a3f5cfa
bemain <bemain.dev@gmail.com>
2025-04-29 11:08:50 +0200

    Begin solving question 3

--- mve302/uppg2.py
+++ mve302/uppg2.py
@@ -3,7 +3,6 @@
 #%% Andra uppgiften
 
 import numpy as np
-from matplotlib import pyplot as plt
 from scipy.stats.distributions import chi2
 from scipy.stats import Normal
 
@@ -16,13 +15,13 @@ def simulate(my=0):
     # Simulera normalfördelad data
     simulated = np.random.normal(my, 1, N)
 
-    Nks=[0]*K
+    Nk=np.zeros(K)
     for x in simulated:
         for k in range(len(xks)):
             if x < xks[k]:
-                Nks[k] += 1
+                Nk[k] += 1
                 break
-    return Nks
+    return Nk
 
 
 
@@ -35,9 +34,9 @@ def run(n=1000, N=100, my=0):
     
     k = 0
     for i in range(n):
-        Nks = simulate(my)
+        Nk = simulate(my)
         # Beräkna avvikelsen
-        T = sum(map(lambda Nk: (Nk-Ek)**2 / Ek, Nks))
+        T = sum((Nk-Ek)**2 / Ek)
         if T >= c:
             k+=1
     return k
@@ -58,3 +57,54 @@ k = run(n, N, 0.1)
 print(f"N(0.1, 1)-fördelning: T => c för {100*k/n}% av försöken (totalt {n} försök) då N={N}")
 
 
+
+#%% Tredje uppgiften
+import numpy as np
+from scipy.stats.distributions import chi2
+from scipy.stats import Normal
+
+my = 100
+sigma = 10
+
+K = 10
+xks = [Normal().icdf((k+1)/K) for k in range(K)]
+
+
+def simulate(N):
+    xi = np.random.normal(my, sigma, N)
+
+    my_hat = sum(xi) / float(N)
+    sigma_hat = np.sqrt(sum((xi - my_hat)**2) / float(N))
+
+    zi = (xi - my_hat) / sigma_hat
+
+    Nk=np.zeros(K)
+    for x in zi:
+        for k in range(len(xks)):
+            if x < xks[k]:
+                Nk[k] += 1
+                break
+    return Nk
+
+
+alpha = 0.05
+c = chi2.ppf(1 - alpha, df=K-3)
+
+def run(N=100, n=1000):
+    # Bestäm väntevärde
+    Ek = N / K  # = N (F(xk) - F(xk-1)) = N(k/K + (k-1)/K) = N/K
+    
+    k = 0
+    for i in range(n):
+        Nk = simulate(N)
+        # Beräkna avvikelsen
+        T = sum((Nk-Ek)**2 / Ek)
+        if T >= c:
+            k+=1
+    print(f"N({my}, {sigma}^2)-fördelning: T => c för {100*k/n}% av försöken (totalt {n} försök)  då N={N}")
+
+
+run(10)
+run(100)
+run(1000)
+run(10000)
